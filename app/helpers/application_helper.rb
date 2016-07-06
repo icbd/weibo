8516ed4d3c426c35f9bc81186b21053f5f1b74ca
cbd <wwwicbd@gmail.com>
2016-07-06 16:02:06 +0800

    full_title helper function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(title_prefix)
+    base_title = "Weibo With ROR5"
+    if title_prefix.empty?
+      base_title
+    else
+      title_prefix + "| #{base_title}"
+    end
+  end
 end
