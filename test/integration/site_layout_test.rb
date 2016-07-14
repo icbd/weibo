4fd5720b72fd41aaa0f5739fd6acc4b7ee659cfa
cbd <wwwicbd@gmail.com>
2016-07-14 16:23:06 +0800

    静态页面布局

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+
+  test "首页链接" do
+    get root_path
+
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", help_path
+
+    assert_select 'title', full_title('')
+  end
+
+
+  test 'full_title 帮助方法' do
+    assert_equal full_title,  @base_title
+    assert_equal full_title(''), @base_title
+    assert_equal full_title('About'), "About | #{@base_title}"
+  end
+
+end
