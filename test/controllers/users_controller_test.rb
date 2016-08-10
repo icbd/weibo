ae97420b294a1f3eefb7b1a89eb0d5f49ec1de7a
cbd <wwwicbd@gmail.com>
2016-08-10 11:19:27 +0800

    test for delete user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user0 = users(:user0)
+    @user0 = users(:user0) #admin
     @user1 = users(:user1)
     @user2 = users(:user2)
   end
@@ -53,5 +53,49 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not @user1.reload.admin?
   end
 
+
+  test "非admin删除用户, 转到root" do
+    log_in_as(@user1)
+    assert_no_difference 'User.count' do
+      delete user_path(@user0)
+    end
+
+    assert_redirected_to root_url
+  end
+
+  test "未登录删除用户, 转到root" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user0)
+    end
+
+    assert_redirected_to login_path
+  end
+
+  test "非管理员看不到删除链接" do
+    log_in_as(@user2)
+    get users_path
+    assert_select 'a', text: '删除', count: 0
+  end
+
+  test "管理员产出用户, 分页展示" do
+    log_in_as(@user0)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+
+    first_page_of_users = User.paginate(page: 1)
+
+    first_page_of_users.each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @user0
+        assert_select 'a[href=?]', user_path(user), text: "删除该用户"
+      end
+    end
+
+
+    assert_difference 'User.count', -1 do
+      delete user_path(@user1)
+    end
+  end
 
 end
